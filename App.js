58148f82adf263c0c312fbbb6039aa40ebf8655e
Eduardo Varella <evarella@gmail.com>
2017-11-23 09:53:45 -0200

    Inital Version - React-native basic application, customizing some color of the basic screen

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={{color: 'white', fontWeight: 'bold'}}>*** GREEN WASH ***</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0f0',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
